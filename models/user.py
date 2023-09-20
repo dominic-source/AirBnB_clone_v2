0c8bdb591e6cc27b654df2a6700c8d76bbea161d
ShinOb1 <obikelv@gmail.com>
2023-09-20 09:43:27 -0700

    update user class

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy import  String, Column, Integer
+from sqlalchemy import  String, Column
 
 
 class User(BaseModel, Base):
