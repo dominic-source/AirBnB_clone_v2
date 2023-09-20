1353ea8c60402b0f46896e6d2e7ec8c5a87cb629
dominic-source <chinonsodomnic@gmail.com>
2023-09-20 09:26:52 -0700

    just merge commits

--- console.py
+++ console.py
@@ -157,7 +157,6 @@ class HBNBCommand(cmd.Cmd):
 
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -239,14 +238,14 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
-
-        print(print_list)
+        if len(print_list) != 0:
+            print(print_list)
 
     def help_all(self):
         """ Help information for the all command """
