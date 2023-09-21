4711f111696cc31aeae797cb804a414d30e57987
dominic-source <chinonsodomnic@gmail.com>
2023-09-21 02:16:37 -0700

    debugging

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     place_id = ""
     user_id = ""
