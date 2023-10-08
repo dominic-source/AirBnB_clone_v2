483a3b2110a3a5282fef313353fb255c1d3eec23
dominic-source <chinonsodomnic@gmail.com>
2023-10-08 14:23:24 -0700

    use puppet to setup static folders and nginx server

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,50 @@
+# Create puppet code
+
+-> package { 'nginx':
+  ensure  => installed
+}
+
+-> file { '/data/web_static/releases/tests':
+  ensure  => directory,
+}
+
+-> file { '/data/web_static/shared/':
+  ensure  => directory,
+}
+
+$cont = "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => present,
+  content => $cont,
+}
+
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test/',
+  force  => true,
+}
+
+-> file { '/data/':
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
+}
+
+$ngconfig = 'location /hbnb_static {
+                alias /data/web_static/current/;
+        }'
+-> file_line { 'find and replace':
+  line  => $ngconfig,
+  after => 'server_name index2.com www.index2.com;',
+}
+
+-> service { 'nginx':
+  ensure => running,
+}
