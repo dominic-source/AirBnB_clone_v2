dd6634a6b6e0635610a185e0784d9e916fa6b5bb
dominic-source <chinonsodomnic@gmail.com>
2023-10-08 13:05:12 -0700

    debug deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,10 +2,10 @@
 """This module houses the function for distributing the versions"""
 
 from fabric.api import env
-from fabric.operations import run, put, local
+from fabric.operations import sudo, put, local
 import os
 
-env.hosts = ['3.94.185.85', '100.24.255.207']
+env.hosts = ['3.94.185.85', '100.24.255.207', 'localhost']
 
 
 def do_deploy(archive_path):
@@ -18,19 +18,19 @@ def do_deploy(archive_path):
         filename = os.path.basename(archive_path)
         dfile = filename.split(".")[0]
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(dfile))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(filename, dfile))
-        run("rm /tmp/{}".format(filename))
-        run(
+        sudo("mkdir -p /data/web_static/releases/{}/".format(dfile))
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+             .format(filename, dfile))
+        sudo("rm /tmp/{}".format(filename))
+        sudo(
             "mv /data/web_static/releases/{}/web_static/* "
             "/data/web_static/releases/{}/"
             .format(dfile, dfile))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(dfile))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current"
-            .format(dfile))
+        sudo("rm -rf /data/web_static/releases/{}/web_static"
+             .format(dfile))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/{} /data/web_static/current"
+             .format(dfile))
         return True
     except Exception:
         return False
