73ca851681eb264ec66fd4c0851e6f75a689f9bc
dominic-source <chinonsodomnic@gmail.com>
2023-10-08 12:17:45 -0700

    debug webstatic deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,27 +11,25 @@ env.hosts = ['3.94.185.85', '100.24.255.207']
 def do_deploy(archive_path):
     """The deployment of the web static files"""
 
-    res = put(archive_path, "/tmp/")
-    if res.failed:
+    if not os.path.exists(archive_path):
         return False
 
-    filename = os.path.basename(archive_path)
-    dfile = filename.split(".")[0]
-    op5 = sudo("mkdir -p /data/web_static/releases/{}/".format(dfile))
-    op1 = sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
-               format(filename, dfile))
-    op2 = sudo("rm /tmp/{}".format(filename))
-    op6 = sudo(
-        "mv /data/web_static/releases/{}/web_static/* "
-        "/data/web_static/releases/{}/"
-        .format(dfile, dfile))
-    op7 = sudo("rm -rf /data/web_static/releases/{}/web_static"
-               .format(dfile))
-    op3 = sudo("rm -rf /data/web_static/current")
-    op4 = sudo("ln -s /data/web_static/releases/{} /data/web_static/current"
-               .format(dfile))
-#    if op1.failed or op2.failed or op3.failed or op4.failed:
-#        return False
-#    if op5.failed or op6.failed or op7.failed or res.failed:
-#        return False
-    return 0
+    try:
+        filename = os.path.basename(archive_path)
+        dfile = filename.split(".")[0]
+        sudo("mkdir -p /data/web_static/releases/{}/".format(dfile))
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+             .format(filename, dfile))
+        sudo("rm /tmp/{}".format(filename))
+        sudo(
+            "mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/"
+            .format(dfile, dfile))
+        sudo("rm -rf /data/web_static/releases/{}/web_static"
+             .format(dfile))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/{} /data/web_static/current"
+             .format(dfile))
+        return True
+    except Exception:
+        return False
