4072459ff322298811494ecaf8ee8c22f56a1950
dominic-source <chinonsodomnic@gmail.com>
2023-10-08 11:23:35 -0700

    debug web static code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,14 +11,13 @@ env.hosts = ['3.94.185.85', '100.24.255.207']
 def do_deploy(archive_path):
     """The deployment of the web static files"""
 
-    local("test -e {}".format(archive_path))
-    result = local("echo $?")
-    if result:
-        return False
     res = put(archive_path, "/tmp/")
+    if res.failed:
+        return False
+
     filename = os.path.basename(archive_path)
     dfile = filename.split(".")[0]
-    op5 = sudo("mkdir -p /data/web_static/releases/{}".format(dfile))
+    op5 = sudo("mkdir -p /data/web_static/releases/{}/".format(dfile))
     op1 = sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
                format(filename, dfile))
     op2 = sudo("rm /tmp/{}".format(filename))
@@ -31,8 +30,8 @@ def do_deploy(archive_path):
     op3 = sudo("rm -rf /data/web_static/current")
     op4 = sudo("ln -s /data/web_static/releases/{} /data/web_static/current"
                .format(dfile))
-    if op1.failed or op2.failed or op3.failed or op4.failed:
-        return False
-    if op5.failed or op6.failed or op7.failed or res.failed:
-        return False
+#    if op1.failed or op2.failed or op3.failed or op4.failed:
+#        return False
+#    if op5.failed or op6.failed or op7.failed or res.failed:
+#        return False
     return True
