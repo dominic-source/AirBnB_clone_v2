df43601cc23f27b9e5375d4a7842f7ef18726274
dominic-source <chinonsodomnic@gmail.com>
2023-10-22 09:44:37 -0700

    find all cities in state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module will start the flask web application
+"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_apps(error):
+    """This will tear down any data"""
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def list_all_cities_in_state():
+    """render templates and jinja implementation"""
+    from models.state import State
+
+    stateList = storage.all(State).values()
+    return render_template('8-cities_by_states.html', stateList=stateList)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
